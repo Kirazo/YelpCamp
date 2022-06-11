e657f72c852efd77c219330a168d116b2fde7ec1
Kirazo <howardtep@hotmail.com>
2022-06-11 16:03:11 -0700

    Added start script and env port

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ app.use((err, req, res, next) => {
         res.send('Something went wrong!')
 })
 
-port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Serving on port 3000')
+    console.log(`Serving on port ${port}`)
 })
